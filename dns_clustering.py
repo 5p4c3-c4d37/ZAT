747de8dbcd57868280170bc0bc2b9f37cb5d5884
sko9370 <sko9370@ymail.com>
2021-01-24 12:38:39 -0500

    save progress, implementing parser to differentiate json vs ascii log formats

--- dns_clustering.py
+++ dns_clustering.py
@@ -1,12 +1,42 @@
 #!/usr/bin/env python3
 
+import os
+import sys
+import argparse
+
 import json
 import pandas as pd
 
-def import_json():
+import math
+from collections import Counter
+
+from zat.log_to_dataframe import LogToDataFrame
+from zat.dataframe_to_matrix import DataFrameToMatrix
+
+from sklearn.ensemble import IsolationForest
+from sklearn.cluster import KMeans
+
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-j', '--json_format', help='Import zeek log in json string format', action='store_true')
+    parser.add_argument('zeek_log_path', type=str, help='Type in location of zeek log')
+
+    args = parser.parse_args()
+    if args.json_format:
+        print('importing zeek log in json format')
+        df = import_json(args.zeek_log_path)
+    else:
+        log_to_df = LogToDataFrame()
+        df = log_to_df.create_dataframe(zeek_log_path)
+
+    return df
+
+def import_json(path):
+    '''
     path = input("Type in the location of the zeek log: ")
     if path == "":
         path = "zeek_logs/NASHUA/dns.log"
+    '''
     # list of json strings, each log entry is a string
     with open(path) as json_file:
         lines = json_file.readlines()
@@ -17,19 +47,12 @@ def import_json():
     df = pd.DataFrame.from_records(map(json.loads, lines))
     return df
 
-import math
-from collections import Counter
-
 def entropy(string):
     p, lns = Counter(string), float(len(string))
     return -sum(count/lns * math.log(count/lns, 2) for count in p.values())
 
-from zat.dataframe_to_matrix import DataFrameToMatrix
-from sklearn.ensemble import IsolationForest
-from sklearn.cluster import KMeans
-
 def main():
-    df = import_json()
+    df = parser()
 
     ######## Preprocessing
 
